9d2bdfd2aec19dfa3c3b9b00690fac0d72a6d5b9
GraceA <govherina@gmail.com>
2023-02-13 16:49:06 -0600

    jsDemo01 commit

---
+++ Ch03/03_05/myDemos/myDemo01.js
@@ -0,0 +1,23 @@
+var myArray = [];
+
+var daysOfTheWeek = ['Sunday','Monday','Tuesday'];
+
+myArray = [0,1,2,'str1','str2',true,false];
+
+var counties = [
+	'Belknap',
+	'Strafford',
+	'Carroll',
+	'Rockingham'
+];
+
+counties[2];
+'Carroll'
+var arrayOfStuff = [
+    {'name' : 'value'},
+    [1,2,3], true, false];
+
+arrayOfStuff;
+
+arrayOfStuff.length;
+4
